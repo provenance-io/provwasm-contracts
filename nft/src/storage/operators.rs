11fb9f8d263d84c86e8ef0fe29795fe966a8d307
kwt <4344285+kwtalley@users.noreply.github.com>
2024-10-31 14:19:49 -0500

    fix nft operator storage types

--- nft/src/storage/operators.rs
+++ nft/src/storage/operators.rs
@@ -3,5 +3,5 @@ use cosmwasm_std::Addr;
 use cw_storage_plus::Map;
 use cw_utils::Expiration;
 
-pub const OPERATORS: Map<'static, (&'static Addr, &'static Addr), Expiration> =
+pub const OPERATORS: Map<(&'static Addr, &'static Addr), Expiration> =
     Map::new(OPERATORS_KEY);
